56ba996b0c55124a70e23965aa32f580bb90fa3c
root <root@ip-172-31-67-229.ec2.internal>
2025-11-25 08:50:54 +0000

    without pull the data from central

--- app.java
+++ app.java
@@ -1,6 +1,7 @@
 <h1>welcome to  github class
   <h2> learning github
 
+<<<<<<< Updated upstream
 ///dev2 pushued data example
   Example
 Input : 145
@@ -8,3 +9,6 @@ Output : It's a Strong Number.
 Explanation : Number = 145
 145 = 1! + 4! + 5!
 145 = 1 + 24 + 120
+=======
+  new code by dev-1 without pull the code
+>>>>>>> Stashed changes
